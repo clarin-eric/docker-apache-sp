f89ae96e88de8abf75d70fc076267824837e19ff
Willem Elbers <willem@clarin.eu>
2016-03-01 14:27:51 +0100

    Improved logging of certificate generation

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+APACHE_NAME="apache2"
 APACHE_CERT_PATH="/etc/apache2/certs"
 APACHE_CERT_CONFIG_FILE="apache_certs.config"
 APACHE_CERT_KEY_FILE="apache.key"
 APACHE_CERT_FILE="apache.crt"
 
+SHIBBOLETH_NAME="shibboleth"
 SHIBBOLETH_CERT_PATH="/etc/shibboleth/certs"
 SHIBBOLETH_CERT_CONFIG_FILE="shibboleth_certs.config"
 SHIBBOLETH_CERT_KEY_FILE="shib.key"
@@ -26,8 +28,9 @@ function prepare_certs {
 
     if [ ! -e ${PATH}/${KEY} ]; then
         /bin/echo "Generating new ${NAME} certificate in: ${PATH}"
+        /bin/mkdir -p /var/log/${NAME} && \
         /bin/mkdir -p ${PATH} && \
-        /usr/bin/openssl req -config ${PATH}/${CONFIG} -new -x509 -days 365 -keyout ${PATH}/${KEY} -out ${PATH}/${CERT} >> /var/log/${NAME}_cert_generation.log 2>&1 && \
+        /usr/bin/openssl req -config ${PATH}/${CONFIG} -new -x509 -days 365 -keyout ${PATH}/${KEY} -out ${PATH}/${CERT} >> /var/log/${NAME}/cert_generation.log 2>&1 && \
         /bin/chmod 0700 ${PATH}/${CERT} && \
         /bin/chmod 0700 ${PATH}/${KEY}
         rc=$?
@@ -41,8 +44,8 @@ function prepare_certs {
 }
 
 /bin/echo "** Preparing certificates"
-prepare_certs "Apache2" ${APACHE_CERT_PATH} ${APACHE_CERT_CONFIG_FILE} ${APACHE_CERT_KEY_FILE} ${APACHE_CERT_FILE}
-prepare_certs "Shibboleth" ${SHIBBOLETH_CERT_PATH} ${SHIBBOLETH_CERT_CONFIG_FILE} ${SHIBBOLETH_CERT_KEY_FILE} ${SHIBBOLETH_CERT_FILE}
+prepare_certs ${APACHE_NAME} ${APACHE_CERT_PATH} ${APACHE_CERT_CONFIG_FILE} ${APACHE_CERT_KEY_FILE} ${APACHE_CERT_FILE}
+prepare_certs ${SHIBBOLETH_NAME} ${SHIBBOLETH_CERT_PATH} ${SHIBBOLETH_CERT_CONFIG_FILE} ${SHIBBOLETH_CERT_KEY_FILE} ${SHIBBOLETH_CERT_FILE}
 
 /bin/echo "** Starting supervisord"
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
